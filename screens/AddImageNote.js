3cec4a81fdf84e7743e01228b055fa9fbab527a8
Szymon Burak <sajmon0031@gmail.com>
2023-06-03 20:46:44 +0200

    Add redux configuration, working on UI

---
+++ screens/AddImageNote.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import { SafeAreaView, View } from "react-native";
+
+const AddAudioNote = () => {
+  return <SafeAreaView>
+    <Container>
+
+    </Container>
+  </SafeAreaView>
+}
+
+const Container = styled(View)`
+  padding: 30px;
+`;
+
+export default AddAudioNote;
